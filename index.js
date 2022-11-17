095af806282c035c328914f68bae2463853d0587
Rinava <lara.mateo@mercadolibre.com>
2022-11-17 13:12:08 -0300

    post resources

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const PORT = 3001;
 const fs = require('fs');
 const path = require('path');
 const pathToFile = path.resolve('./data.json');
+const { v4: uuidv4 } = require('uuid');
 
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile, 'utf8'));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -13,6 +16,25 @@ app.get('/', (req, res) => {
 app.get('/api/resources', (req, res) => {
   const resources = getResources();
   res.send(resources);
+});
+app.post('/api/resources', (req, res) => {
+  const resources = getResources();
+  const newResource = req.body;
+  newResource.createdAt = new Date();
+  newResource.status = 'inactive';
+  newResource.id = uuidv4();
+  resources.push(newResource);
+
+  fs.writeFileSync(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res
+        .status(422)
+        .send({ message: 'Cannot store resource in the file' });
+    }
+    return res.send({
+      message: 'Resource added',
+    });
+  });
 });
 
 app.listen(PORT, () => {
